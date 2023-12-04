cd6ad7dc0873c700a2ad1d08b042002b7a14155d
KirillNaRabote <kirillznaet@mail.ru>
2023-12-04 17:18:25 +0300

    4 лабу сделал

--- 4/lw4/lw4/lw4.cpp
+++ 4/lw4/lw4/lw4.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <windows.h>
 #include <chrono>
+#include <math.h>
+#include <numbers>
 
 using namespace std;
 
@@ -13,38 +15,139 @@ struct BlurImageParam
 
     size_t startRow;
     size_t endRow;
+
+    chrono::steady_clock::time_point startTime;
+
+    int numOfThread;
+
+    int threadPriority;
 };
 
-DWORD WINAPI BlurImage(const LPVOID lpParam)
+DWORD WINAPI BlurImage2(LPVOID lpParam) 
 {
     size_t startRow = (*(BlurImageParam*)lpParam).startRow;
     size_t endRow = (*(BlurImageParam*)lpParam).endRow;
+
     cv::Mat* image = (*(BlurImageParam*)lpParam).image;
     cv::Mat* result = (*(BlurImageParam*)lpParam).result;
 
-    for (int i = startRow; i < endRow; i++)
-    {
-        cv::GaussianBlur((*image).row(i), (*result).row(i), cv::Size(5, 5), 0);
+    chrono::steady_clock::time_point startTime = (*(BlurImageParam*)lpParam).startTime;
+
+    int threadPriority = (*(BlurImageParam*)lpParam).threadPriority;
+
+    int numOfThread = (*(BlurImageParam*)lpParam).numOfThread;
+
+    string outputFileName = "ExecutionTimeForThread#" + to_string(numOfThread) + ".txt";
+    ofstream outputFile(outputFileName);
+
+    int kernelSize = 5;
+    float sigma = 1.0;
+    int halfSize = kernelSize / 2;
+    float twoSigmaSquare = 2 * sigma * sigma;
+    float weightSum = 0.0;
+
+    // Создание ядра Гаусса
+    std::vector<std::vector<float>> kernel(kernelSize, std::vector<float>(kernelSize, 0.0));
+    for (int i = -halfSize; i <= halfSize; i++) {
+        for (int j = -halfSize; j <= halfSize; j++) {
+            float weight = exp(-(i * i + j * j) / twoSigmaSquare) / (3.1415 * twoSigmaSquare);
+            kernel[i + halfSize][j + halfSize] = weight;
+            weightSum += weight;
+        }
+    }
+
+    // Нормализация ядра
+    for (int i = 0; i < kernelSize; i++) {
+        for (int j = 0; j < kernelSize; j++) {
+            kernel[i][j] /= weightSum;
+        }
+    }
+
+    // Применение ядра к изображению
+    for (int i = startRow; i < endRow; i++) {
+        for (int j = 0; j < (*image).cols; j++) {
+            cv::Vec3f sum(0, 0, 0);
+            for (int k = -halfSize; k <= halfSize; k++) {
+                for (int l = -halfSize; l <= halfSize; l++) {
+                    int x = j + l;
+                    int y = i + k;
+                    if (x >= 0 && x < (*image).cols && y >= 0 && y < (*image).rows) {
+                        cv::Vec3b pixel = (*image).at<cv::Vec3b>(y, x);
+                        float weight = kernel[k + halfSize][l + halfSize];
+                        sum += cv::Vec3f(pixel[0], pixel[1], pixel[2]) * weight;
+                    }
+                }
+            }
+            cv::Vec3b blurredPixel(sum[0], sum[1], sum[2]);
+            (*result).at<cv::Vec3b>(i, j) = blurredPixel;
+
+            auto endTime = chrono::high_resolution_clock::now();
+            chrono::duration<double> diff = endTime - startTime;
+
+            /*string msg = to_string(numOfThread) + " | " + to_string(threadPriority) + " | " + to_string(diff.count()) + "\n";*/
+            string msg = to_string(diff.count()) + "\n";
+
+            outputFile << msg;
+        }
     }
 
     ExitThread(0);
 }
 
+void PrintProgramParameters()
+{
+    cout << "Invalid parameters" << endl
+        << "Usage: lw4.exe <InputImage> <OutputImage> <ThreadsNumber> <CoresNumber> <ThreadPriority #1> ... <ThreadPriority #ThreadsNumber>" << endl
+        << "Priority must be [below_normal, normal, above_normal]" << endl;
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc != 5)
+    int threadsNumber;
+
+    if (argc < 5)
     {
-        cout << "Invalid parameters" << endl
-            << "Usage: lw2.exe <InputFileName> <OutputFileName> <ThreadsNumber> <CoresNumber>" << endl;
+        PrintProgramParameters();
         return 1;
     }
+    else
+    {
+        threadsNumber = stoi(argv[3]);
+        if (5 + threadsNumber != argc)
+        {
+            PrintProgramParameters();
+            return 1;
+        }
+    }
 
-    string inFileName = argv[1];
-    string outFileName = argv[2];
-    int threadsNumber = stoi(argv[3]);
+    string inImage = argv[1];
+    string outImage = argv[2];
     int coresNumber = stoi(argv[4]);
+    vector<int> threadsPriority;
+    for (int i = 5; i < 5 + threadsNumber; i++)
+    {
+        string prior = argv[i];
+        if (prior == "below_normal")
+        {
+            threadsPriority.push_back(THREAD_PRIORITY_BELOW_NORMAL);
+        }
+        else if (prior == "normal")
+        {
+            threadsPriority.push_back(THREAD_PRIORITY_NORMAL);
+        }
+        else if (prior == "above_normal")
+        {
+            threadsPriority.push_back(THREAD_PRIORITY_ABOVE_NORMAL);
+        }
+        else
+        {
+            PrintProgramParameters();
+            return 1;
+        }
+    }
 
-    cv::Mat image = cv::imread(inFileName);
+
+    cv::Mat image = cv::imread(inImage);
     if (image.empty())
     {
         cout << "Error loading image!" << endl;
@@ -85,19 +188,23 @@ int main(int argc, char* argv[])
         param.result = &blurredImage;
         param.startRow = startRow;
         param.endRow = endRow;
+        param.numOfThread = i + 1;
+        param.startTime = start;
+        param.threadPriority = threadsPriority[i];
 
         args.push_back(param);
     }
 
     for (size_t i = 0; i < threadsNumber; i++)
     {
-        threads[i] = CreateThread(NULL, 0, &BlurImage, (LPVOID) & (args[i]), CREATE_SUSPENDED, NULL);
+        threads[i] = CreateThread(NULL, 0, &BlurImage2, (LPVOID) & (args[i]), CREATE_SUSPENDED, NULL);
+        SetThreadPriority(threads[i], threadsPriority[i]);
         ResumeThread(threads[i]);
     }
 
     WaitForMultipleObjects(threadsNumber, threads, true, INFINITE);
 
-    cv::imwrite(outFileName, blurredImage);
+    cv::imwrite(outImage, blurredImage);
 
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> diff = end - start;
